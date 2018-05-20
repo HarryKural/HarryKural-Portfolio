134dd266be76bf2ecd69a0b1c3bd79b9d803e397
HarryKural <harrykural333@gmail.com>
2018-05-20 18:22:33 -0400

    modified color scheme & fixed contact form

--- scripts/index.js
+++ scripts/index.js
@@ -148,6 +148,21 @@ $(function() {
 
   setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
 
+    // CONTACT FORM
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "https://formspree.io/harrykural@gmail.com",
+            method: "POST",
+            data: { message: $('form').serialize() },
+            dataType: "json"
+        }).done(function(response) {
+            $('#success').addClass('expand');
+            $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
+        });
+    });
+
   $('#close').click(function() {
     $('#success').removeClass('expand');
   })
